c7481604a1419c8c50af1cbaeaf5744efc8dc303
Joe Bordes <joe@tsolucio.com>
2015-02-14 00:44:27 +0100

    first version of module

---
+++ modules/cbTaxType/language/en_us.lang.php
@@ -0,0 +1,23 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'cbTaxType' => 'Tax Types',
+	'SINGLE_cbTaxType' => 'Tax Type',
+	'cbTaxType ID' => 'Tax Type ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_CBTAXTYPE_INFORMATION' => 'Tax Type Information',
+	'LBL_DESCRIPTION_INFORMATION' => 'Description',
+
+	'taxtypename' => 'Tax Type Name',
+);
+
+?>
